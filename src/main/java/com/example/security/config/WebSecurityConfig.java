848fa69e95d63a57ec6244d21007e2a5cdd01eb8
arthurs22 <arthurscardoso2@gmail.com>
2022-10-27 16:53:01 -0300

    sec config update

--- src/main/java/com/example/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/security/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.example.service.AppUserService;
 
@@ -16,21 +17,21 @@ import lombok.AllArgsConstructor;
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 	
 	private final AppUserService appUserService;
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 		http.csrf().disable().authorizeRequests().antMatchers("/api/v*/registration/**").permitAll().anyRequest()
-				.authenticated().and().formLogin();
+		.authenticated().and().formLogin();
+		return http.build();
 	}
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.authenticationProvider(daoAuthenticationProvider());
-	}
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    	auth.authenticationProvider(daoAuthenticationProvider());
+   }
 	@Bean
 	public DaoAuthenticationProvider daoAuthenticationProvider() {
 		DaoAuthenticationProvider provider = 
